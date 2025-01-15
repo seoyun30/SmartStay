607c1f72973ad89ac08d9c5e35d7b2f7e1b102fc
bora <dianabora@naver.com>
2025-01-15 17:27:06 +0900

    룸서비스 > Menu 케어서비스 > Care 이름 변경

--- src/main/java/com/lookatme/smartstay/Entity/ServiceItem.java
+++ src/main/java/com/lookatme/smartstay/Entity/ServiceItem.java
@@ -1,6 +1,5 @@
 package com.lookatme.smartstay.Entity;
 
-import com.lookatme.smartstay.Constant.OrderState;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Size;
 import lombok.*;
@@ -31,11 +30,11 @@ public class ServiceItem extends BaseEntity {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "menu_num")
-    private RoomService	roomservice; //룸 서비스 조인
+    private Menu menu; //룸 서비스 조인
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "care_num")
-    private CareService	careservice; //룸 케어서비스 조인
+    private Care care; //룸 케어서비스 조인
 
     @ManyToOne
     @JoinColumn(name = "order_num")
