c5b6fceb124ca05e89c7f716fdf7c633ab85b9a9
hyej <hyu34373@gmail.com>
2025-01-20 18:08:06 +0900

    NoticeRepository, NoticeService, NoticeController, notice html 파일 만듬

--- src/main/java/com/lookatme/smartstay/controller/NoticeController.java
+++ src/main/java/com/lookatme/smartstay/controller/NoticeController.java
@@ -3,9 +3,16 @@ package com.lookatme.smartstay.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
 @Log4j2
 public class NoticeController {
+
+    @GetMapping("/List")
+    public String noticeList(){
+
+        return null;
+    }
 }
--- src/main/java/com/lookatme/smartstay/repository/NoticeRepository.java
+++ src/main/java/com/lookatme/smartstay/repository/NoticeRepository.java
@@ -1,8 +1,8 @@
 package com.lookatme.smartstay.repository;
 
-import com.lookatme.smartstay.entity.Cart;
 import com.lookatme.smartstay.entity.Notice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
+
 }
