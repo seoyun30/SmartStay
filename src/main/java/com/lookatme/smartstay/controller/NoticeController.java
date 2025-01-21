f75bb88e2fd3fb1a21c9641a6716504dfb98b0ca
hyej <hyu34373@gmail.com>
2025-01-21 12:00:21 +0900

    NoticeService 수정, NoticeController 작업 중

--- src/main/java/com/lookatme/smartstay/controller/NoticeController.java
+++ src/main/java/com/lookatme/smartstay/controller/NoticeController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
@@ -11,7 +12,43 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class NoticeController {
 
     @GetMapping("/List")
-    public String noticeList(){
+    public String noticeList(PageRequestDTO pageRequestDTO){
+
+        return "notice/noticeList";
+    }
+
+    @GetMapping("/Register")
+    public String noticeRegisterGet(){
+
+        return "notice/noticeRegister";
+    }
+
+    @PostMapping("/Register")
+    public String noticeRegisterPost(){
+
+        return null;
+    }
+
+    @GetMapping("/Read")
+    public String noticeRead(){
+
+        return null;
+    }
+
+    @GetMapping("/Modify")
+    public String noticeModifyGet(){
+
+        return null;
+    }
+
+    @PostMapping("/Modify")
+    public String noticeModifyPost(){
+
+        return null;
+    }
+
+    @PostMapping("/Delete")
+    public String noticeDelete(){
 
         return null;
     }
