c5f7961cb9bd98bf37243382dc87426ab64d8469
bora <dianabora@naver.com>
2025-01-21 13:10:39 +0900

    베이스엔티티 자동화 수정

---
+++ src/main/java/com/lookatme/smartstay/config/AuditorAwareImpl.java
@@ -0,0 +1,23 @@
+package com.lookatme.smartstay.config;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Optional;
+
+public class AuditorAwareImpl implements AuditorAware<String> {
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        Authentication authentication =
+                SecurityContextHolder.getContext().getAuthentication();
+
+        String userId = "";
+        if (authentication != null) {
+            userId = authentication.getName();
+        }
+
+        return Optional.of(userId);
+    }
+}
