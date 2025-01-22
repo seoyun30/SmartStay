046f3e9c8f6655c94f8fccaa70278e62831ddb40
eu <eunununu@naver.com>
2025-01-22 12:41:16 +0900

    CareController MenuController

--- src/main/java/com/lookatme/smartstay/controller/CareController.java
+++ src/main/java/com/lookatme/smartstay/controller/CareController.java
@@ -1,11 +1,110 @@
 package com.lookatme.smartstay.controller;
 
+import com.lookatme.smartstay.dto.CareDTO;
+import com.lookatme.smartstay.dto.MemberDTO;
+import com.lookatme.smartstay.dto.PageRequestDTO;
+import com.lookatme.smartstay.service.CareService;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.security.Principal;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 @Log4j2
+@RequestMapping("/care")
+
 public class CareController {
+
+    private final CareService careService;
+
+    @GetMapping("/careRegister")
+    public String careRegisterGet(Model model, Principal principal){
+
+        model.addAttribute("careDTO", new CareDTO());
+        model.addAttribute("memberDTO", new MemberDTO());
+
+        return "care/careRegister";
+    }
+
+    @PostMapping("/careRegister")
+    public String careRegisterPost(@Valid CareDTO careDTO, BindingResult bindingResult, RedirectAttributes redirectAttributes) throws Exception {
+
+        if (bindingResult.hasErrors()){
+            log.info(bindingResult.getAllErrors());
+
+            return "care/careRegister";
+        }
+
+        Long care_num = careService.careRegister(careDTO);
+        redirectAttributes.addFlashAttribute("msg", "케어서비스가 등록되었습니다. 케어서비스 번호 : " + care_num);
+
+        return "redirect:/care/careRead?care_num=" + care_num;
+    }
+
+    @GetMapping("/careList")
+    public String careList(Model model) {
+
+        List<CareDTO> list = careService.careList();
+        model.addAttribute("list", list);
+
+        return "care/careList";
+    }
+
+    @GetMapping("/careRead")
+    public String careRead(Long care_num, Model model, RedirectAttributes redirectAttributes) {
+
+        try {
+            CareDTO careDTO = careService.careRead(care_num);
+            model.addAttribute("careDTO", careDTO);
+
+            return "care/careRead";
+
+        }catch (EntityNotFoundException e) {
+            redirectAttributes.addFlashAttribute("msg", "존재하지 않는 케어서비스입니다.");
+
+            return "redirect:/care/careList";
+        }
+    }
+
+    @GetMapping("/careModify")
+    public String careModifyGet(Long care_num, Model model) {
+
+        CareDTO careDTO = careService.careRead(care_num);
+
+        if (careDTO != null) {
+            model.addAttribute("careDTO", careDTO);
+
+            return "care/careModify";
+        }else {
+
+            return "redirect:/care/careList";
+        }
+    }
+
+    @PostMapping("/careModify")
+    public String careModifyPost(@Valid CareDTO careDTO, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()){
+            log.info(bindingResult.getAllErrors());
+
+            return "care/careModify";
+        }
+
+        careService.careModify(careDTO);
+
+        return null;
+    }
+
 }
+
---
+++ src/main/java/com/lookatme/smartstay/service/MenuService.java
@@ -0,0 +1,66 @@
+package com.lookatme.smartstay.service;
+
+import com.lookatme.smartstay.dto.MenuDTO;
+import com.lookatme.smartstay.entity.Menu;
+import com.lookatme.smartstay.repository.MenuRepository;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+@Log4j2
+
+public class MenuService {
+
+    private final MenuRepository menuRepository;
+    private final ModelMapper modelMapper;
+    private final ImageService imageService;
+
+    public Long menuRegister(MenuDTO menuDTO) {
+
+        Menu menu = modelMapper.map(menuDTO, Menu.class);
+
+        menu = menuRepository.save(menu);
+
+        return menu.getMenu_num();
+    }
+
+    public MenuDTO menuRead(Long menu_num) {
+
+        Menu menu = menuRepository.findById(menu_num).orElseThrow(EntityNotFoundException::new);
+
+        MenuDTO menuDTO = modelMapper.map(menu, MenuDTO.class);
+
+        return menuDTO;
+    }
+
+    public List<MenuDTO> menuList() {
+
+        List<Menu> menuList = menuRepository.findAll();
+        List<MenuDTO> menuDTOList = menuList.stream()
+                .map(menu -> modelMapper.map(menu, MenuDTO.class))
+                .collect(Collectors.toList());
+
+        return menuDTOList;
+    }
+
+    public MenuDTO menuModify(MenuDTO menuDTO) {
+
+        Menu menu = menuRepository.findById(menuDTO.getMenu_num()).orElseThrow(EntityNotFoundException::new);
+
+        menu.setMenu_name(menuDTO.getMenu_name());
+        menu.setService_info(menuDTO.getService_info());
+        menu.setMenu_detail(menuDTO.getMenu_detail());
+        menu.setMenu_price(menuDTO.getMenu_price());
+
+        return null;
+    }
+}
