a94995b4077290e68c06a22f49c8329eef54bf36
bora <dianabora@naver.com>
2025-02-04 09:44:04 +0900

    카트 서비스 및 룸아이템 레포지토리 수정

--- src/main/java/com/lookatme/smartstay/repository/RoomItemRepository.java
+++ src/main/java/com/lookatme/smartstay/repository/RoomItemRepository.java
@@ -4,15 +4,18 @@ import com.lookatme.smartstay.entity.Pay;
 import com.lookatme.smartstay.entity.Room;
 import com.lookatme.smartstay.entity.RoomItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface RoomItemRepository extends JpaRepository<RoomItem, Long> {
 
     //내 장바구니에 담긴 아이템 찾기
-    public RoomItem findByCartIdAndRoom_num (Long cart_num, Long room_num);
+    @Query("select r from RoomItem r where r.cart.cart_num = :cart_num and r.room.room_num = :room_num")
+    public RoomItem findByCart_Cart_numAndRoom_num (Long cart_num, Long room_num);
 
-    public List<RoomItem> findByCartId (Long cart_id);
+    @Query("select r from RoomItem r where r.cart.cart_num = :cart_num")
+    public List<RoomItem> findByCart_Cart_num (Long cart_num);
 
     //추후 룸서비스 버전도 생성 예정
 
