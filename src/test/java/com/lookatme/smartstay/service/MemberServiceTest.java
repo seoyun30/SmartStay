fe315f7d4722380aee004f043a1f8774976906c6
yuri <hyr0813@naver.com>
2025-02-12 18:08:25 +0900

    회원가입시 power 기본값 설정, 관리자 로그인시 권한 승인전 로그인제한

---
+++ src/test/java/com/lookatme/smartstay/service/MemberServiceTest.java
@@ -0,0 +1,26 @@
+package com.lookatme.smartstay.service;
+
+import com.lookatme.smartstay.dto.PageRequestDTO;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Log4j2
+class MemberServiceTest {
+
+    @Autowired
+    private MemberService memberService;
+
+    @Test
+    public void testMemberService() {
+
+        PageRequestDTO pageRequestDTO = new PageRequestDTO();
+        memberService.memberList(pageRequestDTO);
+
+    }
+
+}
