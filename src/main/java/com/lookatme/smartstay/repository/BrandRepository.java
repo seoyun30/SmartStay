059a11b74cd24e879b38f169b2fd195619ba57e2
seoyun30 <simply9507@naver.com>
2025-02-13 16:08:38 +0900

    myBrand만 끌어오기, barnd,hotel 리스트 모양 조절

--- src/main/java/com/lookatme/smartstay/repository/BrandRepository.java
+++ src/main/java/com/lookatme/smartstay/repository/BrandRepository.java
@@ -2,6 +2,12 @@ package com.lookatme.smartstay.repository;
 
 import com.lookatme.smartstay.entity.Brand;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface BrandRepository extends JpaRepository<Brand, Long> {
+
+    @Query("select b from Brand b where b.create_by = :email")
+    public List<Brand> findByEmail(String email);
 }
