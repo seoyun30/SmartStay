63c61bee4b4a88ab53b1f96c4ffb98e3e61b4a38
eu <eunununu@naver.com>
2025-02-17 17:12:27 +0900

    룸서비스 주문 페이지 구현

--- src/main/java/com/lookatme/smartstay/controller/OrderReserveController.java
+++ src/main/java/com/lookatme/smartstay/controller/OrderReserveController.java
@@ -1,15 +1,20 @@
 package com.lookatme.smartstay.controller;
 
 import com.lookatme.smartstay.dto.HotelDTO;
+import com.lookatme.smartstay.dto.ImageDTO;
 import com.lookatme.smartstay.dto.OrderItemDTO;
 import com.lookatme.smartstay.entity.Care;
+import com.lookatme.smartstay.entity.Image;
 import com.lookatme.smartstay.entity.Menu;
 import com.lookatme.smartstay.entity.OrderReserve;
+import com.lookatme.smartstay.repository.ImageRepository;
 import com.lookatme.smartstay.service.HotelService;
+import com.lookatme.smartstay.service.OrderItemService;
 import com.lookatme.smartstay.service.OrderReserveService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -19,7 +24,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Controller
 @RequiredArgsConstructor
@@ -28,23 +36,43 @@ import java.util.List;
 public class OrderReserveController {
     private final OrderReserveService orderReserveService;
     private final HotelService hotelService;
+    private final OrderItemService orderItemService;
+    private final ImageRepository imageRepository;
+    private final ModelMapper modelMapper;
 
     @GetMapping("/orderReserveRegister")
     public String orderReserveRegisterGet(Model model) {
 
         List<Menu> menus = orderReserveService.getAllMenus();
-
         List<Care> cares = orderReserveService.getAllCares();
+
+        Map<Long, List<ImageDTO>> menuImagesMap = new HashMap<>();
+        for (Menu menu : menus) {
+            List<Image> menuImages = imageRepository.findByTarget("menu", menu.getMenu_num());
+            List<ImageDTO> menuImagesDTOs = menuImages.stream()
+                    .map(image -> modelMapper.map(image, ImageDTO.class)).collect(Collectors.toList());
+            menuImagesMap.put(menu.getMenu_num(), menuImagesDTOs);
+        }
+
+        Map<Long, List<ImageDTO>> careImagesMap = new HashMap<>();
+        for (Care care : cares) {
+            List<Image> careImages = imageRepository.findByTarget("care", care.getCare_num());
+            List<ImageDTO> careImageDTOs = careImages.stream()
+                    .map(image -> modelMapper.map(image, ImageDTO.class)).collect(Collectors.toList());
+            careImagesMap.put(care.getCare_num(), careImageDTOs);
+        }
 
         model.addAttribute("menus", menus);
         model.addAttribute("cares", cares);
+        model.addAttribute("menuImagesMap", menuImagesMap);
+        model.addAttribute("careImagesMap", careImagesMap);
 
         return "orderreserve/orderReserveRegister";
     }
 
 
     @PostMapping("/orderReserveRegister")
-    public String orederReserveRegisterPost(@Valid OrderItemDTO orderItemDTO,
+    public String orederReserveRegisterPost(@Valid OrderItemDTO orderItemDTO, Model model,
                                             BindingResult bindingResult, Principal principal) {
 
         if (bindingResult.hasErrors()) {
@@ -58,7 +86,21 @@ public class OrderReserveController {
             return sb.toString();
         }
 
-        return "/pay";
+        try {
+            orderItemService.createOrder(orderItemDTO);
+        }catch (Exception e) {
+            model.addAttribute("errorMessage", e.getMessage());
+            return "orderreserve/orderReserveRegister";
+        }
+        model.addAttribute("successMessage", "룸서비스 주문 성공");
+
+        return "orderreserve/pay";
+    }
+
+    @PostMapping("/pay")
+    public String pay() {
+
+        return "orderreserve/pay";
     }
 
 
