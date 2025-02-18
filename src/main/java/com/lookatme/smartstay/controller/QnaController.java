7b0221fac397574d1f002be1f89f4d3e5b10b932
seoyun30 <simply9507@naver.com>
2025-02-18 18:10:14 +0900

    Qna 관련 정보 변경

--- src/main/java/com/lookatme/smartstay/controller/QnaController.java
+++ src/main/java/com/lookatme/smartstay/controller/QnaController.java
@@ -110,7 +110,7 @@ public class QnaController {
             return "redirect:/qna/qnaList";
         }
 
-        return "/qna/qnaModify";
+        return "qna/qnaModify";
     }
 
     @PostMapping("/qnaModify")
