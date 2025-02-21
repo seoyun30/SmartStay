4f9aa538dbfd569f3cc0a688afb499113db829c6
eu <eunununu@naver.com>
2025-02-21 14:53:24 +0900

    사이드바에 권한,아이디,브랜드/호텔 정보 모든 페이지에서 뜨게 수정, 브랜드/호텔 상세보기에서 footer 가려지지 않게 수정, 호텔/브랜드 이미지 기능 추가, 전체적인 레이아웃 수정

---
+++ src/main/java/com/lookatme/smartstay/controller/GlobalControllerAdvice.java
@@ -0,0 +1,68 @@
+package com.lookatme.smartstay.controller;
+
+import com.lookatme.smartstay.entity.Member;
+import com.lookatme.smartstay.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@ControllerAdvice
+public class GlobalControllerAdvice {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @ModelAttribute("superAdmin")
+    public String superAdmin() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.isAuthenticated()) {
+            String role = authentication.getAuthorities().iterator().next().getAuthority();
+            if ("SUPERADMIN".equals(role)) {
+                return "SUPERADMIN";
+            }
+        }
+        return null;
+    }
+
+    @ModelAttribute("adminRole")
+    public String adminRole() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.isAuthenticated()) {
+            return authentication.getAuthorities().iterator().next().getAuthority();
+        }
+        return "";
+    }
+
+    @ModelAttribute("adminEmail")
+    public String adminEmail() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.isAuthenticated()) {
+            return authentication.getName();
+        }
+        return "";
+    }
+
+    @ModelAttribute("brandName")
+    public String brandName() {
+        String email = adminEmail();
+        Member member = memberRepository.findByEmail(email);
+        if (member != null && member.getBrand() != null) {
+            return member.getBrand().getBrand_name();
+        }
+        return "";
+    }
+
+    @ModelAttribute("hotelName")
+    public String hotelName() {
+        String email = adminEmail();
+        Member member = memberRepository.findByEmail(email);
+        if (member != null && member.getHotel() != null) {
+            return member.getHotel().getHotel_name();
+        }
+        return "";
+    }
+
+
+}
