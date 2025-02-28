524e2504e718a70b5896b1ede5a0cb5186b30964
bora <dianabora@naver.com>
2025-02-28 18:11:06 +0900

    유저 마이페이지 룸예약 및 룸서비스 목록에서 상세보기 추가 및 룸서비스 상세보기 추가

--- src/main/java/com/lookatme/smartstay/repository/OrderReserveItemRepository.java
+++ src/main/java/com/lookatme/smartstay/repository/OrderReserveItemRepository.java
@@ -8,7 +8,13 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface OrderReserveItemRepository extends JpaRepository<OrderReserveItem, Long> {
 
+    @Query("select o from OrderReserveItem o where o.roomReserveItem.room.hotel.hotel_num = :hotel_num")
+    Page<OrderReserveItem> findByHotelPage (Long hotel_num, Pageable pageable);
+
     @Query("select o from OrderReserveItem o where o.orderReserve.member.email = :email")
     Page<OrderReserveItem> findByEmailPage (String email, Pageable pageable);
+
+    @Query("select o from OrderReserveItem o where o.serviceitem_num = :serviceitem_num and o.orderReserve.member.email = :email")
+    OrderReserveItem findByServiceitemNumAndEmail(Long serviceitem_num, String email);
 
 }
