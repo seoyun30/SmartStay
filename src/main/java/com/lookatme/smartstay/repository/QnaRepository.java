740c30ec46ac14efd3c1b222aab333912d60e724
seoyun30 <simply9507@naver.com>
2025-03-05 18:44:49 +0900

    qna 호텔정보, 마이페이지, 사용자별 정보 끌어오기

--- src/main/java/com/lookatme/smartstay/repository/QnaRepository.java
+++ src/main/java/com/lookatme/smartstay/repository/QnaRepository.java
@@ -1,5 +1,6 @@
 package com.lookatme.smartstay.repository;
 
+import com.lookatme.smartstay.entity.Brand;
 import com.lookatme.smartstay.entity.Cart;
 import com.lookatme.smartstay.entity.Qna;
 import com.lookatme.smartstay.entity.Room;
@@ -17,8 +18,16 @@ public interface QnaRepository extends JpaRepository<Qna, Long> {
     @Query("SELECT q FROM Qna q WHERE q.writer = :writer")
     List<Qna> findByWriter(@Param("writer") String writer);  // 작성자로 QnA 목록 조회
 
+    //호텔로 검색 페이징
+    @Query("select q from Qna q where q.hotel.hotel_num = :hotel_num")
+    Page<Qna> findbyHotel(Long hotel_num, Pageable pageable);
+
+    //브랜드로 검색 페이징
+    @Query("select q from Qna q where q.hotel.brand.brand_num = :brand_num")
+    Page<Qna> findbyBrand(Long brand_num, Pageable pageable);
+
     @Query("SELECT q FROM Qna q WHERE q.writer = :writer")
-    List<Qna> findByWriter(@Param("writer") String writer, Pageable pageable);  // 작성자로 QnA 목록 조회 페이징
+    Page<Qna> findByWriter(@Param("writer") String writer, Pageable pageable);  // 작성자로 QnA 목록 조회 페이징
 
     @Query("select q from Qna q")
     Page<Qna> selcetAll(Pageable pageable); //페이징처리 목록 전부
@@ -36,6 +45,10 @@ public interface QnaRepository extends JpaRepository<Qna, Long> {
     @Query("select q from Qna q where q.writer like concat('%', :str, '%') ")
     public Page<Qna> selectlikeWriter(String str, Pageable pageable);
 
+    //호텔명 검색
+    @Query("select q from Qna q join q.hotel h where h.hotel_name like concat('%', :str, '%')")
+    public Page<Qna> selectlikeHotel(String str, Pageable pageable);
+
     @Query("select q from Qna q where q.title like concat('%', :str, '%') or q.content like concat('%', :str, '%') ")
     public Page<Qna> titleOrCon(String str, Pageable pageable);
 
@@ -47,6 +60,23 @@ public interface QnaRepository extends JpaRepository<Qna, Long> {
 
     @Query("select q from Qna q where q.title like concat('%', :str, '%') or q.content like concat('%', :str, '%') or q.writer like concat('%', :str, '%') ")
     public Page<Qna> titleOrConOrWr(String str, Pageable pageable);
+
+    //mylist용
+    @Query("select q from Qna q where q.title like concat('%', :keyword, '%')  and q.writer = :email ")
+    public Page<Qna> findByTitleAndWriterContaining(String keyword, String email, Pageable pageable);
+
+    @Query("select q from Qna q where q.content like concat('%', :keyword, '%')  and q.writer = :email ")
+    public Page<Qna> findByContentAndWriterContaining(String keyword, String email, Pageable pageable);
+
+    @Query("select q from Qna q join q.hotel h where h.hotel_name like concat('%', :str, '%') and q.writer = :email ")
+    public Page<Qna> selectlikeHotelAndWriter(String str, String email, Pageable pageable);
+
+    @Query("select q from Qna q where q.title like concat('%', :str, '%') or q.content like concat('%', :str, '%') and q.writer = :email ")
+    public Page<Qna> titleOrConAndWriter(String str, String email, Pageable pageable);
+
+
+
+
 
    /* @Query("SELECT r FROM Room r WHERE LOWER(r.room_name) LIKE LOWER(CONCAT('%', :roomName, '%'))")
     List<Room> findByRoomNameContainingIgnoreCase(@Param("roomName") String roomName, Sort sort);
