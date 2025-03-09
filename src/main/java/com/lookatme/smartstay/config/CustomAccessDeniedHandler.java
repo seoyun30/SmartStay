454cbc9e448f17db882f9d579a8da06b20f1715a
허유리 <hyr0813@naver.com>
2025-03-09 21:52:27 +0900

    시큐리티 오류 수정

---
+++ src/main/java/com/lookatme/smartstay/config/CustomAccessDeniedHandler.java
@@ -0,0 +1,35 @@
+package com.lookatme.smartstay.config;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Iterator;
+
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+
+
+        //권한에 걸리면 무조건 제한
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iter = authorities.iterator();
+        GrantedAuthority auth = iter.next();
+        String role = auth.getAuthority();
+        System.out.println(role);
+
+
+
+        request.getSession().invalidate();
+        response.sendRedirect("/member/login");
+    }
+}
