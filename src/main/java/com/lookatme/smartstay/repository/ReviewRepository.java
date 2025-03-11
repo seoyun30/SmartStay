0cbbf5fb54bb600325ff51862efa8ff0ca069e8a
hyej <hyu34373@gmail.com>
2025-03-11 18:15:24 +0900

    관리자 리뷰 페이지 구현 페이지네이션 기능 추가

--- src/main/java/com/lookatme/smartstay/repository/ReviewRepository.java
+++ src/main/java/com/lookatme/smartstay/repository/ReviewRepository.java
@@ -1,47 +1,64 @@
 package com.lookatme.smartstay.repository;
 
+import com.lookatme.smartstay.constant.CheckState;
+import com.lookatme.smartstay.entity.Hotel;
 import com.lookatme.smartstay.entity.Review;
+import com.lookatme.smartstay.entity.RoomReserveItem;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
+@Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
-
 
     //리뷰 리스트
     // 해당 호텔의 모든 리뷰조회
     @Query("select r from Review r where r.hotel.hotel_num =:hotel_num")
     List<Review> findByHotel(Long hotel_num);
 
-    // 브랜드의 모든 리뷰조회
-    @Query("select r from Review r where r.hotel.brand.brand_num =:brand_num")
-    List<Review> findByHotelorBrand(@Param("brand") Long brand_num);
+    //페이지 형식으로 호텔 리스트
+    @Query("select r from  Review r where r.hotel =:hotel")
+    Page<Review> findByAdHotel(@Param("hotel") Hotel hotel, Pageable pageable);
 
     // 해당 유저의 모든 리뷰조회(이메일을 통해 조회)
     @Query("select r from Review r where r.member.email = :email")
     List<Review> findByUser(@Param("email") String email);
 
+//    // 브랜드의 모든 리뷰조회 (보류)
+//    @Query("select r from Review r where r.hotel.brand.brand_num =:brand_num")
+//    List<Review> findByHotelorBrand(@Param("brand") Long brand_num);
 
-//    /**카테고리 name 으로 review 찾기**/
-//    Page<Review> findAllByOrderByCountDesc(Pageable pageable);  //조회수
+    // 검색 기능
+    @Query("SELECT r FROM Review r WHERE r.room.hotel.hotel_num = :hotel_num "
+            + "AND (:hotel_name IS NULL OR r.room.hotel.hotel_name LIKE %:hotel_name%) "
+            + "AND (:room_name IS NULL OR r.room.room_name LIKE %:room_name%) ")
+    Page<Review> findReviewBySearch(
+            @Param("hotel_num") Long hotel_num,
+            @Param("hotel_name") String hotel_name,
+            @Param("room_name") String room_name,
+            Pageable pageable);
+
+    /**리뷰를 시간순, 별점 순, ,.. 으로 조회하기 **/
+//    @Query("SELECT r FROM Review r ORDER BY r.reg_date DESC ")
+//    Page<Review> findByOrderByReg_dateDesc(Pageable pageable);
+
+//    Page<Review> findAllByOrderByCountDesc(Pageable pageable);  //조회수 // 아직
 //
-//    Page<Review> findAllByOrderByViewsDesc(Pageable pageable);  // 조회순
+//    Page<Review> findAllByOrderByViewsDesc(Pageable pageable);  // 조회순  // 아직
 //
 //    Page<Review> findAllByOrderByDateTimeDesc(Pageable pageable); // 시간순
 //
 //    Page<Review> findAllByOrderByReviewLikeDesc(Pageable pageable);  // 별점 순
 
 //    // Acs : 오름차, Desc : 내림차
-//    List<Review> findByScoreOrderByScoreDesc(String score);
-//    List<Review> findByScoreOrderByScoreAsc(String score);
-
-//    List<Review> findByReg_dateContaining(String reg_date);
-//
-//    List<Review> findByScoreContaining(String score);
-
+    @Query("SELECT r FROM Review r")
+    Page<Review> findAllReviews(Pageable pageable);
 
     //
     @Query("SELECT r FROM Review r WHERE r.hotel.hotel_num = :hotel_num ORDER BY r.reg_date DESC")
